0ee20ca9d1fa7a27c9d5049b5a39037a3bcb05c2
Diego Marquez <diegomarquezlive@live.com>
2023-09-02 12:06:47 -0700

    added schema managing tool DbUp

--- ResponsiBoss.DbUp/Program.cs
+++ ResponsiBoss.DbUp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var connectionString = args.FirstOrDefault() ?? "Server=(local);Database=ResponsiBoss;Trusted_Connection=True;Trust Server Certificate=True";
+
+var upgrader = DbUp.DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
