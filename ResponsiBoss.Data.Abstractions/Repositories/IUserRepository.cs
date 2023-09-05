ca4d0e167a61619219864eb83a61bd883c3fd003
Diego Marquez <diegomarquezlive@live.com>
2023-09-04 20:15:26 -0700

    got rid of genericrepository to have more freedom/visibility of my queries

--- ResponsiBoss.Data.Abstractions/Repositories/IUserRepository.cs
+++ ResponsiBoss.Data.Abstractions/Repositories/IUserRepository.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace ResponsiBoss.Data.Abstractions
 {
-    public interface IUserRepository : IGenericRepository<UserProfile, Guid>
+    public interface IUserRepository
     {
+        Task<UserProfile> FindByIdAsync(Guid id);
+        Task<IEnumerable<UserProfile>> GetAllAsync();
+        Task<Guid> CreateAsync(UserProfile entity);
+        Task<Guid> DeleteAsync(Guid id);
+        Task<Guid> UpdateAsync(UserProfile entity);
     }
 }
--- ResponsiBoss.Data/Repositories/UserRepository.cs
+++ ResponsiBoss.Data/Repositories/UserRepository.cs
@@ -1,4 +1,4 @@
-﻿using ResponsiBoss.Data.Abstractions;
+﻿using Dapper;
 using ResponsiBoss.Data.Models;
 using System;
 using System.Collections.Generic;
@@ -8,11 +8,63 @@ using System.Threading.Tasks;
 
 namespace ResponsiBoss.Data
 {
-    public class UserRepository : GenericRepository<UserProfile, Guid>, Abstractions.IUserRepository
+    public class UserRepository : RepositoryBase,  Abstractions.IUserRepository
     {
-        public UserRepository(IDataContext dataContext)
-            :base(dataContext)
+        public UserRepository(Abstractions.IDataContext dataContext)
+            : base (dataContext)
         {
+        }
+
+        public async Task<Guid> CreateAsync(UserProfile entity)
+        {
+            string sql = @"INSERT INTO [User] ([UserId], [Username], [Password], [EmailAddress], [PhoneNumber], [CreatedOn])
+                           VALUES (@UserId, @Username, @Password, @EmailAddress, @PhoneNumber, @CreatedOn)";
+
+            await base.DbConnection.ExecuteScalarAsync(sql, entity);
+
+            return entity.UserId;
+        }
+
+        public async Task<Guid> DeleteAsync(Guid userId)
+        {
+            string sql = @"DELETE FROM [User]
+                           WHERE UserId = @UserId";
+
+            await base.DbConnection.ExecuteScalarAsync(sql, new { UserId = userId });
+
+            return userId;
+        }
+
+        public async Task<UserProfile> FindByIdAsync(Guid userId)
+        {
+            var sql = @"SELECT u.*
+                        FROM [User] AS u
+                        WHERE u.UserId = @UserId";
+
+            var result = await base.DbConnection.QuerySingleAsync<UserProfile>(sql, new { UserId = userId });
+
+            return result;
+        }
+
+        public async Task<IEnumerable<UserProfile>> GetAllAsync()
+        {
+            var sql = @"SELECT u.*
+                        FROM [User] AS u";
+
+            var users = await base.DbConnection.QueryAsync<UserProfile>(sql);
+
+            return users;
+        }
+
+        public async Task<Guid> UpdateAsync(UserProfile entity)
+        {
+            string sql = @"UPDATE [User]
+                           SET [Username] = @Username, [Password] = @Password, [EmailAddress] = @EmailAddress, [PhoneNumber] = @PhoneNumber
+                           WHERE [UserId] = @UserId";
+            
+            var userId = await base.DbConnection.ExecuteScalarAsync<Guid>(sql, entity);
+
+            return userId;
         }
     }
 }
