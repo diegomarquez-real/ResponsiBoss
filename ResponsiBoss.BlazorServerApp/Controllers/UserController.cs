9ed1c0758dbba1374375d8d897aa3070835085f4
Diego Marquez <diegomarquezlive@live.com>
2023-09-21 17:17:00 -0700

    user logout implemented

--- ResponsiBoss.BlazorServerApp/Controllers/UserController.cs
+++ ResponsiBoss.BlazorServerApp/Controllers/UserController.cs
@@ -8,17 +8,20 @@ namespace ResponsiBoss.BlazorServerApp.Controllers
     public class UserController : Controller
     {
         private readonly IApplicationSignInManager _applicationSignInManager;
+        private readonly IApplicationSignOutManager _applicationSignOutManager;
         private readonly CustomStorage _customStorage;
 
         public UserController(IApplicationSignInManager applicationSignInManager,
+            IApplicationSignOutManager applicationSignOutManager,
             CustomStorage customStorage)
         {
             _applicationSignInManager = applicationSignInManager;
+            _applicationSignOutManager = applicationSignOutManager;
             _customStorage = customStorage;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Login(Guid key)
+        public async Task<IActionResult> LoginAsync(Guid key)
         {
             var userSession = _customStorage.UserSession.ReadEncryptedItem<UserSession>(key);
 
@@ -31,6 +34,14 @@ namespace ResponsiBoss.BlazorServerApp.Controllers
                 _customStorage.UserSession.Remove(key);
 
             return Redirect("/");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogoutAsync()
+        {
+            await _applicationSignOutManager.SignOutAsync();
+
+            return Redirect("/Logout");
         }
     }
 }
